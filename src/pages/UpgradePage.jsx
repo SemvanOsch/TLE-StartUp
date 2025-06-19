5a4562aeaf63103bffdc9657f70ecc9c6a3da60b
JTS-Games <1066300@hr.nl>
2025-06-19 13:38:57 +0200

    Commit

--- src/pages/UpgradePage.jsx
+++ src/pages/UpgradePage.jsx
@@ -29,6 +29,8 @@ import background8 from '/src/images/background8.png'
 import background9 from '/src/images/background9.png'
 import background10 from '/src/images/background10.png'
 import '/src/index.css'
+import SterrenBG from "../component/sterrenBG.jsx";
+
 
 
 const UpgradePage = () => {
@@ -290,22 +292,28 @@ const UpgradePage = () => {
         setCurrentButton(money >= 14 ? upgradeButton20 : upgradeButton20disabled);
     }, [money]);
 
-    if (loading) return null
+    // if (loading) return null
     return(
-        <div className="relative min-h-screen bg-background text-text flex flex-col items-center justify-center overflow-hidden">
+        <div
+            className=" relative min-h-screen bg-background text-text flex flex-col items-center justify-center overflow-hidden">
+
+
             {/* Fade-in white overlay */}
             <div
                 className={`fixed inset-0 bg-white z-50 transition-opacity duration-1000 pointer-events-none ${
                     fadeOverlayVisible ? 'opacity-100' : 'opacity-0'
                 }`}
             />
-            <div style={{backgroundImage: `url(${background})`, backgroundSize: 'cover', backgroundPosition: 'center', minHeight: '100vh', minWidth: '100vw'}}>
+            <div className="bg-cover bg-center min-h-screen min-w-full z-40" style={{backgroundImage: `url(${background})`}}>
                 <h1 className={'money'}>${money}</h1>
                 <img id={'upgradeButton'} src={currentButton} onClick={useMoney}/>
                 <button onClick={addMoney}>+money</button>
                 <button onClick={handleLoguout}>logout</button>
             </div>
+            <SterrenBG/>
         </div>
+
+
     );
 }
 
